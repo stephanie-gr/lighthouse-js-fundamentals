c25cbce05ee7c6d3e34efa508a6992153b15cf21
Stephanie Grant <stephanie.grant.2008@gmail.com>
2021-06-10 15:03:54 +0000

    re-do to fix nested folder mistake

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+const temperature = 12;
+
+if (temperature < 0) {
+    console.log("Pick out a scarf.");
+ } else if (temperature < 15) {
+     console.log("Short sleeves won't cut it.");
+ } else {
+     console.log("short sleeves are fine.");
+ };
+
+
+ const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
+}
+
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+}
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea");
+}
+
+
